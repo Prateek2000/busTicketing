9d4eb937c9bfc9061d96c047d74bf4ae3efe49d2
Prateek Sharma <43009939+Prateek2000@users.noreply.github.com>
2023-07-02 17:55:29 +0000

    Added db connnection code, resolved ssl connection issue with RDS, app now successfully connects to db. endpoints and queries up next

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+//export NODE_TLS_REJECT_UNAUTHORIZED='0'
 const express = require("express");
+const { Client } = require("pg")
+const env = require("dotenv").config()
 
 const app = express();
 const port = 3000;
+
+const client = new Client({
+    user: `${process.env.DB_USER}`,
+    host: `${process.env.DB_HOST}`,
+    database: `${process.env.DB_NAME}`,
+    password: `${process.env.DB_PASSWORD}`,
+    port: `${process.env.DB_PORT}`,
+    ssl: true
+})
+client.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected");
+});
